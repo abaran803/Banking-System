fc15c422bdb1d3a5766b8bbf693fb2d7bb3bbfb2
Ayush Baranwal <abaran803@gmail.com>
2022-06-27 18:47:25 +0530

    UI Improvements

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 app.use(routes);
